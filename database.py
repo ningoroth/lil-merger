e5a0d0dc391b56976d8fb539bd7d4b53ec604758
BuildTools <unconfigured@null.spigotmc.org>
2023-05-07 14:31:31 +0200

    update

--- database.py
+++ database.py
@@ -13,10 +13,10 @@ else:
     os.makedirs(data_path, exist_ok=True)
 
     db = sqlite3.connect(data_path + filename)
-    db.execute('CREATE TABLE IF NOT EXISTS Recipies (id INTEGER PRIMARY KEY, result TEXT, element1 TEXT, element2 TEXT)')
+    db.execute('CREATE TABLE IF NOT EXISTS Recipies \
+              (id INTEGER PRIMARY KEY, result TEXT, element1 TEXT, element2 TEXT)')
     db.close()
 
     print("Database Created")
 
 
-    
